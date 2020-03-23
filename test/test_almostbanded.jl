139c0a47607e7fdb406e65dbc61cad382c97aeb3
Sheehan Olver <solver@mac.com>
2020-03-23 08:54:27 +0000

    update tests

--- test/test_almostbanded.jl
+++ test/test_almostbanded.jl
@@ -1,4 +1,4 @@
-using SemiseparableMatrices, BandedMatrices, FillArrays, Test, LinearAlgebra, MatrixFactorizations, LazyArrays, ArrayLayouts
+using SemiseparableMatrices, BandedMatrices, Test, LinearAlgebra, MatrixFactorizations, LazyArrays, ArrayLayouts
 import BandedMatrices: _BandedMatrix, _banded_qr!
 import SemiseparableMatrices: bandpart, fillpart, AlmostBandedLayout
 import MatrixFactorizations: QRPackedQ
@@ -6,7 +6,7 @@ import MatrixFactorizations: QRPackedQ
 @testset "AlmostBandedMatrix" begin
     @testset "Slices" begin
         n = 80
-        A = AlmostBandedMatrix(BandedMatrix(Fill(2.0,n,n),(1,1)), LowRankMatrix(fill(3.0,n), ones(1,n)))
+        A = AlmostBandedMatrix(BandedMatrix(fill(2.0,n,n),(1,1)), LowRankMatrix(fill(3.0,n), ones(1,n)))
         V = view(A,1:3,1:3)
         @test MemoryLayout(typeof(A)) == MemoryLayout(typeof(V)) == AlmostBandedLayout()
         @test AlmostBandedMatrix(V) == A[1:3,1:3] == V
@@ -15,7 +15,7 @@ import MatrixFactorizations: QRPackedQ
 
     @testset "Triangular" begin
         n = 80
-        A = AlmostBandedMatrix(BandedMatrix(Fill(2.0,n,n),(1,1)), LowRankMatrix(fill(3.0,n), ones(1,n))) 
+        A = AlmostBandedMatrix(BandedMatrix(fill(2.0,n,n),(1,1)), LowRankMatrix(fill(3.0,n), ones(1,n))) 
         b = randn(n)
         @test MemoryLayout(typeof(UpperTriangular(A))) == TriangularLayout{'U','N',AlmostBandedLayout}()
         @test UpperTriangular(Matrix(A)) \ b ≈ UpperTriangular(A) \ b
@@ -26,7 +26,7 @@ import MatrixFactorizations: QRPackedQ
 
     @testset "QR" begin
         n = 80
-        A = AlmostBandedMatrix(BandedMatrix(Fill(2.0,n,n),(1,1)), LowRankMatrix(fill(3.0,n), ones(1,n)))
+        A = AlmostBandedMatrix(BandedMatrix(fill(2.0,n,n),(1,1)), LowRankMatrix(fill(3.0,n), ones(1,n)))
         A[band(0)] .+= 1:n
         Ã = deepcopy(A)
         B,L = bandpart(A),fillpart(A)
--- test/test_semiseparable.jl
+++ test/test_semiseparable.jl
@@ -1,15 +1,17 @@
-using SemiseparableMatrices, LowRankApprox, BandedMatrices, LinearAlgebra, Test
+using SemiseparableMatrices, BandedMatrices, LinearAlgebra, Test
+
+include("test_almostbanded.jl")
 
 # Constructions:
-@testset "construction" begin
-    let n = 10, v = rand(n, 2), w = rand(n, 2), y = rand(n, 3), z = rand(n, 3), U = LowRankMatrix(v, w), L = LowRankMatrix(y, z)
-        @test SemiseparableMatrix(U, L, 1, 2) isa SemiseparableMatrix{Float64}
-        @test Matrix(SemiseparableMatrix(U, L, 1, 2)) == triu(U, 2)+tril(L, -3)
-        @test_throws AssertionError SemiseparableMatrix(U, L, 10, 2)
-        @test_throws AssertionError SemiseparableMatrix(U, L, 1, 10)
-    end
+# @testset "construction" begin
+#     let n = 10, v = rand(n, 2), w = rand(n, 2), y = rand(n, 3), z = rand(n, 3), U = LowRankMatrix(v, w), L = LowRankMatrix(y, z)
+#         @test SemiseparableMatrix(U, L, 1, 2) isa SemiseparableMatrix{Float64}
+#         @test Matrix(SemiseparableMatrix(U, L, 1, 2)) == triu(U, 2)+tril(L, -3)
+#         @test_throws AssertionError SemiseparableMatrix(U, L, 10, 2)
+#         @test_throws AssertionError SemiseparableMatrix(U, L, 1, 10)
+#     end
 
-    let v = rand(n, 2), w = rand(n, 2), y = rand(n+1, 3), z = rand(n+1, 3), U = LowRankMatrix(v, w), L = LowRankMatrix(y, z)
-        @test_throws AssertionError SemiseparableMatrix(U, L, 1, 2)
-    end
-end
+#     let v = rand(n, 2), w = rand(n, 2), y = rand(n+1, 3), z = rand(n+1, 3), U = LowRankMatrix(v, w), L = LowRankMatrix(y, z)
+#         @test_throws AssertionError SemiseparableMatrix(U, L, 1, 2)
+#     end
+# end
